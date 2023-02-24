bf698831e8d601978e093934d69ab2326aa43fb9
Konstantin Annikov <annikovk@gmail.com>
2023-02-24 11:14:07 +0100

    Added "PeriodicYouTrackRequest.timers" entity to fix shadow ongoing timers and too much requests

--- src/com.jetbrains.youtrack.sdPlugin/app.js
+++ src/com.jetbrains.youtrack.sdPlugin/app.js
@@ -2,19 +2,21 @@
 /// <reference path="libs/js/stream-deck.js" />
 
 const myAction = new Action('com.jetbrains.youtrack.get-ticket-count');
-myAction.cache = {}
-myAction.polls = {}
+myAction.activeTiles = {}
 
-function APIRequest(context, settings) {
-    const youTrack = {
+class PeriodicYouTrackRequest {
+    settings
+    context
+    //todo: convert youTrack to class
+    youTrack = {
         Ready: false,
         token: "",
         url: "",
         searchQuery: "",
         GetTicketsCount: async function () {
             try {
-                if (youTrack.Ready) {
-                    return await youTrack.RunQuery(youTrack.url, youTrack.token, youTrack.searchQuery);
+                if (this.Ready) {
+                    return await this.RunQuery(this.url, this.token, this.searchQuery);
                 } else {
                     return -1;
                 }
@@ -24,7 +26,7 @@ function APIRequest(context, settings) {
             }
         },
         RunQuery: async function (url, token, query) {
-            url = url + '/api/issues?fields=idReadable,summary,votes,customFields(projectCustomField(field(name)),value(name))&$top=500&query=' + youTrack.EscapeQuery(query)
+            url = url + '/api/issues?fields=idReadable,summary,votes,customFields(projectCustomField(field(name)),value(name))&$top=500&query=' + this.EscapeQuery(query)
             console.log("executing: " + query);
             let response = await fetch(url, {
                 method: 'GET',
@@ -40,102 +42,146 @@ function APIRequest(context, settings) {
             return query.replace(/ /g, '+').replace(/#/g, '%23');
         },
         OpenPage: function () {
-            let url = youTrack.url + '/issues?q=' + youTrack.EscapeQuery(youTrack.searchQuery)
+            let url = this.url + '/issues?q=' + this.EscapeQuery(this.searchQuery)
             $SD.openUrl(url);
         }
     };
-    let firstRequestFinished = false;
-
-    async function sendRequest() {
-        try {
-            let ticketCount = await youTrack.GetTicketsCount();
-            let count = ticketCount.toString();
-
-            if (settings["hide-zero"] === "on" && ticketCount === 0) {
-                count = "";
-            } else if (ticketCount === -1) {
-                count = "N/A";
-            }
-
-            let name = settings["yt-search-name"] || "";
-            let title = name.length > 0 ? name + "\n" + count : count;
-            firstRequestFinished = true;
-            console.log("updating title: " + title);
-            $SD.setTitle(context, title);
-        } catch (e) {
-            console.error(e)
-        }
-    }
-
-    function restartPeriodicPoll() {
-        destroy();
-        firstRequestFinished = false;
-        youTrack.token = settings["yt-token"];
-        youTrack.url = settings["yt-url"];
-        youTrack.searchQuery = settings["yt-search-query"];
-        youTrack.Ready = !!(youTrack.url && youTrack.token && youTrack.searchQuery);
-        refreshTitleWhilePolling(Math.floor(Math.random() * 3000));
-        startPollingLoop(Math.floor(Math.random() * 5) + 1);
-    }
-
-    function startPollingLoop(wait) {
-        myAction.polls[context] = setTimeout(async () => {
-            await sendRequest()
-            let delay = settings["refresh-interval"] || 60
-            startPollingLoop(delay);
-        }, wait * 1000);
-    }
-
-    function refreshTitleWhilePolling(wait = 1000) {
-        console.log("refreshing title while polling in " + wait);
-        let dots = ""
-
-        function sleep(wait) {
-            return new Promise(resolve => setTimeout(resolve, wait));
-        }
-
-        sleep(wait).then(() => {
-            let id = setInterval(() => {
-                if (!firstRequestFinished) {
-                    dots += ".";
-                    if (dots.length > 3) {
-                        dots = "";
-                    }
-                    let name = settings["yt-search-name"] || "";
-                    let title = name.length > 0 ? name + "\n" + dots : dots;
-                    $SD.setTitle(context, title);
-                } else {
-                    clearInterval(id);
+    timers = {
+        activeTimers: [],
+        addTimer: function (name, timer) {
+            this.activeTimers.push({name: name, timer: timer});
+        },
+        newPeriodicTask: function (name, callback, delay, initialDelay = Math.floor(Math.random() * 1000)) {
+            let tick = async () => {
+                await callback();
+                if (this.timerIdExists(timer)) {
+                    setTimeout(tick, delay);
                 }
-            }, 1000);
-        });
-    }
+            }
+            let timer = setTimeout(tick, initialDelay);
+            this.addTimer(name, timer);
 
-    function destroy() {
-        if (myAction.polls[context]) {
-            clearTimeout(myAction.polls[context]);
-            myAction.polls[context] = {};
+        },
+        timerIdExists: function (id) {
+            return this.activeTimers.some(x => x.timer === id);
+        },
+        timerNameExists: function (name) {
+            return this.activeTimers.some(x => x.name === name);
+        },
+        stopAll: function () {
+            this.activeTimers.forEach(timer => {
+                clearInterval(timer.timer);
+                clearTimeout(timer.timer);
+            })
+            this.activeTimers = [];
+        },
+        stopByName: function (name) {
+            for (let activeTimersKey in this.activeTimers) {
+                if (this.activeTimers[activeTimersKey].name === name) {
+                    clearInterval(this.activeTimers[activeTimersKey].timer);
+                    clearTimeout(this.activeTimers[activeTimersKey].timer);
+                    this.activeTimers.splice(activeTimersKey, 1);
+                }
+            }
+        },
+    }
+    updateSettings = (new_settings) => {
+        this.settings = new_settings;
+        this.settings["last-result"] = "";
+        this.restart()
+    }
+    destroy() {
+        this.timers.stopAll();
+    }
+    restart() {
+        this.destroy()
+        this.start();
+    }
+    start() {
+        let restartPeriodicPoll = () =>{
+            this.destroy();
+            IndicateOngoingRefresh();
+            this.timers.newPeriodicTask("periodic-poll", refreshValue, this.settings["refresh-interval"] * 1000 || 60 * 1000);
         }
+        let sendRequest = async () =>{
+            try {
+                let ticketCount = await this.youTrack.GetTicketsCount();
+                let count = ticketCount.toString();
+                if (this.settings["hide-zero"] === "on" && ticketCount === 0) {
+                    count = "";
+                } else if (ticketCount === -1) {
+                    count = -1;
+                }
+                return count;
+            } catch (e) {
+                console.error("Could not get ticket count: ");
+                console.error(e)
+                return -1;
+            }
+        }
+
+        let refreshValue = async () => {
+            let ticketCount = await sendRequest()
+            this.timers.stopByName( "refreshIndicator")
+            if (ticketCount !== -1) {
+                updateLastResult(ticketCount)
+                setTileValue(ticketCount)
+            } else {
+                setTileValue("!ERR")
+            }
+        }
+
+        let updateLastResult = (ticketCount) => {
+            this.settings["last-result"] = ticketCount;
+            $SD.setSettings(this.context, this.settings);
+        }
+
+        let IndicateOngoingRefresh = (wait = 500) =>{
+            let dots = "";
+            let title = "";
+            let indicatorFunction = () => {
+                dots += ".";
+                if (dots.length > 3) {
+                    dots = "";
+                }
+                // If there is a cached result, blinks the title while polling (indicating that the cached result is shown)
+                if (this.settings["last-result"] && this.settings["last-result"] !== "" && this.settings["last-result"] !== undefined) {
+                    if (dots.length % 2 !== 0) {
+                        title = this.settings["last-result"];
+                    } else {
+                        title = "♻"
+                    }
+                } else {
+                    title = dots;
+                }
+                if (this.timers.timerNameExists("refreshIndicator")) {
+                    setTileValue(title);
+                }
+            }
+            this.timers.newPeriodicTask("refreshIndicator", indicatorFunction, wait);
+        }
+
+        let setTileValue = (str) => {
+            let name = this.settings["yt-search-name"] || "";
+            let title = name.length > 0 ? name + "\n" + str : str;
+            // console.log("setting title to: " + title.replace(/\n/g, "\\n"));
+            $SD.setTitle(this.context, title);
+        }
+        this.fillYTSettings(this.settings);
+        restartPeriodicPoll()
     }
-
-    function updateSettings(new_settings) {
-        settings = new_settings;
-        restartPeriodicPoll();
+    //todo: move fillYTSettings to youTrack object
+    fillYTSettings = () => {
+        this.youTrack.token = this.settings["yt-token"];
+        this.youTrack.url = this.settings["yt-url"];
+        this.youTrack.searchQuery = this.settings["yt-search-query"];
+        this.youTrack.Ready = !!(this.youTrack.url && this.youTrack.token && this.youTrack.searchQuery);
     }
-
-    function OpenPage() {
-        youTrack.OpenPage();
+    constructor(context, settings) {
+        this.settings = settings;
+        this.context = context;
+        this.start();
     }
-
-    restartPeriodicPoll();
-
-    return {
-        sendRequest: sendRequest,
-        updateSettings: updateSettings,
-        destroy: destroy,
-        OpenPage: OpenPage
-    };
-
 }
 
 /**
@@ -146,34 +192,33 @@ $SD.onConnected(({actionInfo, appInfo, connection, messageType, port, uuid}) => 
 });
 
 myAction.onKeyUp(({action, context, device, event, payload}) => {
-    const api_request = myAction.cache[context];
-    if (!api_request)
+    const api_request = myAction.activeTiles[context];
+    if (!api_request || !api_request.youTrack.Ready)
         $SD.showAlert(context);
     else
-        api_request.OpenPage();
+        api_request.youTrack.OpenPage();
 });
 myAction.onDidReceiveSettings(({action, context, device, event, payload}) => {
     const settings = payload.settings;
-    const api_request = myAction.cache[context];
+    const api_request = myAction.activeTiles[context];
 
     if (!settings || !api_request) return;
 
     api_request.updateSettings(settings);
-    myAction.cache[context] = api_request;
 })
 myAction.onSendToPlugin(async ({action, context, device, event, payload}) => {
     if (action === 'com.jetbrains.youtrack.get-ticket-count') {
-        myAction.cache[context] = new APIRequest(context, payload.settings);
+        myAction.activeTiles[context] = new PeriodicYouTrackRequest(context, payload.settings);
     }
 
 });
 myAction.onWillAppear(async ({action, context, device, event, payload}) => {
-    myAction.cache[context] = new APIRequest(context, payload.settings);
+    myAction.activeTiles[context] = new PeriodicYouTrackRequest(context, payload.settings);
 })
 myAction.onWillDisappear(async ({action, context, device, event, payload}) => {
-    let api_request = myAction.cache[context];
-    if (api_request) {
-        api_request.destroy();
-        delete myAction.cache[context];
+    let yt_request = myAction.activeTiles[context];
+    if (yt_request) {
+        yt_request.destroy();
+        delete myAction.activeTiles[context];
     }
 })
